7ef68f73189739840b7b70f62ff1b101f52ae613
alloyking1 <alloyking1@gmail.com>
2020-12-17 10:41:12 -0500

    deploying to heroku

--- src/store.js
+++ src/store.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import Category from "./api/Category.api"
 
 Vue.use(Vuex);
 
@@ -8,23 +7,4 @@ export default new Vuex.Store({
   state: {
     user: "",
   },
-
-  actions: {
-    /**fetch user on login */
-    async fetchCategories({ commit }) {
-      const res = await Category.fetchCategories;
-      return console.log(res);
-      // commit("UPDATE_USER_INFO", res.data.data);
-      // return;
-    },
-  },
-
-  mutations: {
-    //update the state with user details
-    // UPDATE_USER_INFO(state, payload) {
-    //   state.user = payload;
-    //   return;
-    // },
-  },
-
 });
